9c3bbcc5146df1abe290582b68f5a2758f40160d
doski-codes <kevin.dodor@yahoo.com>
2021-04-29 08:10:08 +0000

    Completed reverse number

---
+++ reverse_number.py
@@ -0,0 +1,55 @@
+'''
+Given a signed 32-bit integer x, return x with its digits reversed.
+If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1],
+then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+Example 1:
+
+Input: x = 123
+Output: 321
+
+Example 2:
+
+Input: x = -123
+Output: -321
+
+Example 3:
+
+Input: x = 120
+Output: 21
+
+Example 4:
+
+Input: x = 0
+Output: 0
+
+Constraints:
+
+-231 <= x <= 231 - 1
+'''
+
+def reverse(x):
+    num = 0
+    signed = False
+
+    if x < 0:
+        x = x * -1
+        signed = True
+
+    while x:
+        value = x % 10
+        x = x // 10
+        num = (num * 10) + value
+
+    if signed:
+        return num * -1
+    else:
+        return num
+
+def main():
+    print(reverse(123))
+
+if __name__ == '__main__':
+    main()
